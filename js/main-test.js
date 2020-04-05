9bde99ba65e74cc810fef6fa384d6edf9f63f5bf
georgiylityagin <geolit94@gmail.com>
2020-04-05 23:31:31 +0300

    test disable multimediaScript

--- js/main-test.js
+++ js/main-test.js
@@ -1,13 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const multimediaScript = document.createElement('script');
-  multimediaScript.setAttribute('src','./js/multimedia.js');
-  const buttonsContainer = document.querySelector('.buttons-wrap');
+// window.addEventListener('DOMContentLoaded', () => {
+//   const multimediaScript = document.createElement('script');
+//   multimediaScript.setAttribute('src','./js/multimedia.js');
+//   const buttonsContainer = document.querySelector('.buttons-wrap');
 
-  buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-      '<button class="button">Нет</button>';
+//   buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
+//       '<button class="button">Нет</button>';
   
-  document.querySelector('.multimedia').appendChild(multimediaScript);
-});
+//   document.querySelector('.multimedia').appendChild(multimediaScript);
+// });
 
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector(".buttons-wrap");
